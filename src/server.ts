dd62b6e4b4506121fe75b1008f8ffbf010b293fe
Kleber Coelho <111254436+kleberclh@users.noreply.github.com>
2025-04-09 10:28:49 -0300

    #

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 // src/server.ts
 import app from './app';
 
-const port = process.env.PORT || 4001;
+const port = process.env.PORT
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
